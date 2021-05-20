e09b5f6b17e97f658f4747aa4466feb40c7ca14f
pablo <pmorenocf@alumnos.unex.es>
2021-05-20 16:40:53 +0200

    Notation correction

--- trotter_based_methods.py
+++ trotter_based_methods.py
@@ -26,7 +26,7 @@ class Trotter_based_methods:
 
         P_failure = 6*eps_tot
 
-        r = ((27*np.pi**2/2)*(lambd/deltaE)**2) / P_failure**3
+        n = ((27*np.pi**2/2)*(lambd/deltaE)**2) / P_failure**3
 
         #delta = deltaE/(2*lambd)
         #q = np.log2(1/delta)-1
@@ -42,11 +42,11 @@ class Trotter_based_methods:
         
         # error in individual rotations
         #epsilon_SS = (eps_tot/(2*np.pi*(2**m-1)))**2/N # Same as below. The N comes from Jordan Wigner implementation
-        epsilon_SS = epsilon_S/(r*N) # from eq 39 same as the one above
+        epsilon_SS = epsilon_S/(n*N) # from eq 39 same as the one above
 
         rost_cost_factor = N*self.tools.c_rotation_synthesis(epsilon_SS)
         
-        return rost_cost_factor*r
+        return rost_cost_factor*n
 
     # For the randomised Hamiltonian approach, the equations are similar. However, now $p_f = 3/4P_f$ and ,
     # $$n = 8\\Gamma^2\\left(\\frac{ \\pi^3 \\Lambda^3}{8\\delta_E^3}\\right)^{1/2}\\left(\\frac{1+p_f}{p_f}\\right)^{3/2}\\frac{1}{\\epsilon_{tot}^{1/2}} 
@@ -58,7 +58,7 @@ class Trotter_based_methods:
 
         P_failure = 8*eps_tot
 
-        r = 4.35*np.sqrt(8)*(np.pi*Lambd/deltaE)**(3/2) *(Gamma/ P_failure)**2
+        n = 4.35*np.sqrt(8)*(np.pi*Lambd/deltaE)**(3/2) *(Gamma/ P_failure)**2
 
         # error in individual rotations
         #Lambda_A = Lambd/(2*lambd)
@@ -81,8 +81,8 @@ class Trotter_based_methods:
         #n2 = 8*Gamma**2 * ( 2**(m+1)*np.pi**3*Lambda_A**3/eps_tot_2  )**(1/2) *2*(2**m-1)
         #n3 = 8*Gamma**2 * ( 2**(m+1)*np.pi**3*Lambda_A**3/eps_tot_3  )**(1/2) *2*(2**m-1)
 
-        epsilon_SS = epsilon_S/(r*N) # N comes from Jordan-Wigner, n is the number of U applied
+        epsilon_SS = epsilon_S/(n*N) # N comes from Jordan-Wigner, n is the number of U applied
 
         rost_cost_factor = N*self.tools.c_rotation_synthesis(epsilon_SS)
         
-        return rost_cost_factor*r
+        return rost_cost_factor*n
