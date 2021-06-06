e313abaaa71f175fba0de0330ed919edffab9fe0
pablo <pmorenocf@alumnos.unex.es>
2021-06-06 19:52:07 +0200

    Correction of the QROAM method

--- qrom_methods.py
+++ qrom_methods.py
@@ -39,7 +39,7 @@ class QROM_methods:
         Subprepare = QROM_cost(3*M**D) + uniform_cost(3) + D*uniform_cost(M) + 2*compare + (3+D*np.log2(M))*Fredkin_cost
         Prepare = Subprepare + D*uniform_cost(M, controlled=True) + D*np.log2(M)*Fredkin_cost + sum + 2*self.tools.multi_controlled_not(np.log2(N))
         
-        Select = 3*QROM_cost(N) + 2*np.log2(N)*Fredkin_cost
+        Select = 3*QROM_cost(N) + 2*np.ceil(np.log2(N))*Fredkin_cost
         
         Reflexion = self.tools.multi_controlled_not(2*np.log2(N)+2*mu + N)
         return r*(2*Prepare + Reflexion + Select) #todo: rotations in the quantum walk
@@ -74,26 +74,33 @@ class QROM_methods:
             possible_results = np.floor(np.log2(x)), np.ceil(np.log2(x))
             return min(possible_results, key= lambda z: abs(x-2**z))
 
-        Amplitude_amplification = 2*3*2*uniform_cost(N/2) + uniform_cost(L) + 2*3*rot_synt + 2*self.tools.multi_controlled_not(np.ceil(np.log2(N))) +2*2*compare
+        # In front of uniform_cost(N/2), there is a multiplier of 3 due to Amplitude amplification, and a multiple of 4 due to p, q, r, s
+        Amplitude_amplification = 2*3*(2*uniform_cost(N/2)) + 2*3*rot_synt+ 2*3*compare + 2*self.tools.multi_controlled_not(np.ceil(np.log2(N)))
 
         if sparsity_d is not None:
             d = sparsity_d
         else:
-            d = L*(N**2/8 + N/4)
+            d = (2*L+1)*(N**2/8 + N/4)
         M = np.log(N**2) + mu
         kc = 2**closest_power(np.sqrt(d/M))
         ku = 2**closest_power(np.sqrt(d))
         QROAM = 4*(np.ceil(d/kc)+4*M*(kc-1)+2*np.ceil(d/ku) + 4*ku)
-        
+
         compare = self.tools.compare_cost(mu)
         Fredkin_cost = 4 # The controlled swaps = 1 Toffoli
+        controlled_swap_L = np.ceil(np.log2(L))*Fredkin_cost
+
+        Step_1_state_preparation = uniform_cost(L) + QROM_cost(L) + compare + controlled_swap_L
+        Step_2_state_preparation = self.tools.multi_controlled_not(np.ceil(np.log2(L)))
+
+        controlled_swap_p_q = np.ceil(np.log2(N**2/4))*Fredkin_cost
         
         sum = self.tools.sum_cost(np.ceil(np.log2(N/2)))
         mult = self.tools.multiplication_cost(np.ceil(np.log2(N/2)))
         continuous_register = 2*mult + 3*sum
 
         # In the same order as depicted in figure 11 in PHYS. REV. X 8, 041015
-        Prepare =  Amplitude_amplification + continuous_register + QROAM + compare + np.ceil(np.log2(L*N**2/4))*Fredkin_cost
+        Prepare = Amplitude_amplification + Step_1_state_preparation + Step_2_state_preparation + 2*continuous_register + QROAM + 2*(compare + controlled_swap_p_q)
 
         Select = 2*(2*QROM_cost(N) + 2*2*self.tools.multi_controlled_not(np.log2(N))) # The initial 2 is due to Select_1 and Select_2. See figure 1 in original article.
 
